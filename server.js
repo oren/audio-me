5cb2b951b79e5d018fab88c3592bfd604a615950
Andrew Kim <akim@yp.com>
2013-12-20 15:25:14 -0800

    First attempt at the index file

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require('http');
+var fs = require('fs');
+var indexHtml = fs.readFileSync(__dirname + '/index.html');
+
 http.createServer(function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello World\n');
+      res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(indexHtml);
 }).listen(3000, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:3000/');
