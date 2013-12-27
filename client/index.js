4c0f19b01941a1017bcc0472ccfa0a8a3482a70b
Andrew Kim <akim@yp.com>
2013-12-27 12:28:40 -0800

    Fixing errors

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 'use strict';
 var html5Audio = require('./html5audio.js');
 var Recorder = require('./recorder.js');
+var recorder;
 
 window.AudioContext = window.AudioContext ||
                           window.webkitAudioContext;
@@ -14,14 +15,27 @@ function initAudio(err, stream) {
     inputPoint = context.createGain();
     var microphone = context.createMediaStreamSource(stream);
     microphone.connect(inputPoint);
-    audioRecorder = new Recorder( inputPoint );
-    zeroGain = audioContext.createGain();
+    recorder = new Recorder( inputPoint, {workerPath: "/static/recorderWorker.js"} );
+    var zeroGain = context.createGain();
     zeroGain.gain.value = 0.0;
     inputPoint.connect( zeroGain );
-    zeroGain.connect( audioContext.destination );
+    zeroGain.connect( context.destination );
 }
+
+var recordBtn;
+var stopBtn;
 
 window.addEventListener('load', function(e) {
     console.log('Asking for audio');
     html5Audio(initAudio);
+    recordBtn = document.getElementById('btn-record');
+    stopBtn = document.getElementById('btn-stop');
+    recordBtn.addEventListener('click', function(e) {
+        console.log('recording');
+    });
+
+    stopBtn.addEventListener('click', function(e) {
+        console.log('stop');
+    });
+
 });
